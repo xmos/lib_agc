a6cc44a722ccd9263369c289f7248a63b55e564f
Dan Follent <danielf@xmos.com>
2018-12-14 15:18:07 +0000

    py model driven by VAD. WIP.

--- python/agc.py
+++ python/agc.py
@@ -1,32 +1,122 @@
 # Copyright (c) 2018, XMOS Ltd, All rights reserved
-
+from math import sqrt
 import numpy as np
+
+def get_factor(dBps):
+    return float(10 ** (dBps*0.015/20))
+
+def get_frame_energy(frame):
+    energy = np.iinfo(np.int32).max * np.power(frame, 2).sum()
+    return energy
+
+def dBFS_to_int32(dBFS):
+    return np.iinfo(np.int32).max * (10 ** (float(min(dBFS, agc.DBFS_MAX))/20))
+
 
 class agc:
 
     INIT_GAIN = 1
     FIXED_LIMIT = 0.5
+    WAIT_COUNT = 0 #66 * 0.3
+    DBFS_MAX = 0
+    MAX_GAIN = 10 ** float(127/20)
+    MIN_GAIN = 10 ** float(-127/20)
+    AGC_LOOK_AHEAD_FRAMES = 0
 
-    def __init__(self,
-            channel_count,
-            frame_advance):
+    def __init__(self, channel_count, desired_dBFS, desired_Error, frame_advance):
         self.channel_count = channel_count
         self.frame_advance = frame_advance
         self.gain = [agc.INIT_GAIN] * channel_count
+        self.state = ['STABLE'] * channel_count
+        self.up_factor = get_factor(10)
+        self.down_factor = get_factor(-20)
+        self.wait_count = [agc.WAIT_COUNT] * channel_count
+        self.desired = dBFS_to_int32(desired_dBFS)
+        self.desired_max = dBFS_to_int32(desired_dBFS + desired_Error)
+        self.desired_min = dBFS_to_int32(desired_dBFS - desired_Error)
+
+        self.frame_energy = np.zeros((1 + agc.AGC_LOOK_AHEAD_FRAMES,))
+        self.sample_buffer = np.zeros((channel_count, self.frame_advance * (1 + agc.AGC_LOOK_AHEAD_FRAMES)))
+
+        print "desired: {} (/{})".format(self.desired, np.finfo(np.float64).max)
+        print "up_factor: {}".format(self.up_factor)
+        print "down_factor: {}".format(self.down_factor)
+
         return
 
-    def set_channel_gain(self, ch_index, gain_db):
-        self.gain[ch_index] = gain_db
+
+    def set_channel_gain(self, ch_index, gain):
+        self.gain[ch_index] = gain
         if self.gain[ch_index] < 1:
             self.gain[ch_index] = 1
         return
 
-    def process_frame(self, input_frame, verbose = False):
+
+    def multiply_gain(self, factor):
+        self.gain = self.gain * factor
+
+
+    def adapt(self, input_frame, ch_index, frame_energy):
+        energy = self.gain[ch_index] * frame_energy
+        if self.state[ch_index] == 'STABLE':
+            if energy > self.desired_max:
+                self.state[ch_index] = 'DOWN'
+            elif energy < self.desired_min:
+                self.state[ch_index] = 'WAIT'
+
+        elif self.state[ch_index] == 'UP':
+            self.gain[ch_index] = min(agc.MAX_GAIN, self.gain[ch_index] * self.up_factor)
+            if energy > self.desired:
+                self.state[ch_index] = 'STABLE'
+
+        elif self.state[ch_index] == 'DOWN':
+            self.gain[ch_index] = max(agc.MIN_GAIN, self.gain[ch_index] * self.down_factor)
+            if energy < self.desired:
+                self.state[ch_index] = 'STABLE'
+
+        elif self.state[ch_index] == 'WAIT':
+            if energy > self.desired_max:
+                self.state[ch_index] = 'DOWN'
+            elif energy < self.desired_min:
+                if (self.wait_count[ch_index] <= 0):
+                    self.wait_count[ch_index] = agc.WAIT_COUNT
+                    self.state[ch_index] = 'UP'
+                else:
+                    self.wait_count[ch_index] -= 1
+            else:
+                self.state[ch_index] = 'STABLE'
+        else:
+            print "ERROR Unknown state: {}".format(self.state[ch_index])
+
+
+    def process_frame(self, input_frame, vad = False, verbose = False):
+
         def limit_gain(x):
             return x if abs(x) < agc.FIXED_LIMIT else (np.sign(x) * 2 * agc.FIXED_LIMIT - agc.FIXED_LIMIT ** 2 / x)
 
+        # print np.shape(input_frame)
+        # print np.shape(self.sample_buffer)
+
+        # Push new frame energy to the back
+        self.frame_energy[:-1] = self.frame_energy[1:]
+        self.frame_energy[-1] = get_frame_energy(input_frame[0])
+
+        # Push new frame to the back of sample buffer
+        for ch in range(self.channel_count):
+            for n in range(len(self.sample_buffer[0, ]) - self.frame_advance):
+                self.sample_buffer[ch, n] = self.sample_buffer[ch, n + self.frame_advance]
+
+            for n in range(self.frame_advance):
+                self.sample_buffer[ch,-self.frame_advance + n] = input_frame[ch, n]
+
         output = np.empty(np.shape(input_frame))
-        for i, ch in enumerate(input_frame):
-            gained_input = self.gain[i] * ch
-            output[i] = [limit_gain(x) for x in gained_input]
+        if vad == True:
+            for i, input_ch_frame in enumerate(input_frame):
+                self.adapt(input_frame, i, np.mean(self.frame_energy)) # Adapt on average of look-ahead frame energies
+
+        for ch in range(self.channel_count):
+            for n in range(self.frame_advance):
+                gained_sample = self.gain[ch] * self.sample_buffer[ch, n]
+                output[ch, n] = limit_gain(gained_sample)
+
         return output
--- python/test_wav_agc.py
+++ python/test_wav_agc.py
@@ -4,22 +4,27 @@ import sys
 
 package_dir = os.path.dirname(os.path.abspath(__file__))
 path1 = os.path.join(package_dir,'../../audio_test_tools/python/')
+path2 = os.path.join(package_dir,'../../lib_vad/python/')
 sys.path.append(path1)
+sys.path.append(path2)
 
 import numpy as np
 import scipy.io.wavfile
 import audio_utils as au
-from agc import agc
+import agc
+import vad
 import argparse
 
 FRAME_ADVANCE = 240
 
-AGC_GAIN_CH0 = 20
-AGC_GAIN_CH1 = 1
+# AGC_GAIN_CH0 = 20
+# AGC_GAIN_CH1 = 1
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="inpout wav file")
+    parser.add_argument("--gain0", type=int, default=20)
+    parser.add_argument("--gain1", type=int, default=1)
     parser.add_argument("output", help="output wav file")
     parser.parse_args()
     args = parser.parse_args()
@@ -35,13 +40,43 @@ if __name__ == "__main__":
 
     output = np.zeros((channel_count, file_length))
 
-    gc = agc(channel_count, FRAME_ADVANCE)
-    gc.set_channel_gain(0, AGC_GAIN_CH0)
-    gc.set_channel_gain(1, AGC_GAIN_CH1)
+    agc = agc.agc(channel_count, -10, 6, FRAME_ADVANCE)
+    agc.set_channel_gain(0, args.gain0)
+    agc.set_channel_gain(1, args.gain1)
 
+    vad = vad.vad()
 
+    print agc.gain[0]
+    energy = []
+    gain0 = []
+    gain1 = []
+    vad_results = []
     for frame_start in range(0, file_length-FRAME_ADVANCE, FRAME_ADVANCE):
         x = au.get_frame(wav_data, frame_start, FRAME_ADVANCE)
-        output[:, frame_start: frame_start + FRAME_ADVANCE] = gc.process_frame(x, verbose = False)
+        vad_frame = vad.run(x[0])
+        vad_results.append(vad_frame)
+        output[:, frame_start: frame_start + FRAME_ADVANCE] = agc.process_frame(x, vad_frame > 0.4, verbose = False)
+        gain0.append(agc.gain[0])
+        gain1.append(agc.gain[1])
+        energy.append(np.mean(agc.frame_energy))
+
+    import matplotlib.pyplot as plt
 
     scipy.io.wavfile.write(args.output, rate, output.T)
+
+    x_axis = [x * 240.0/16000.0 for x in range(len(vad_results))]
+    plt.figure(1)
+    ax1 = plt.subplot(411)
+    ax1.set_title("Frame Energy")
+    plt.plot(x_axis, energy)
+    ax2 = plt.subplot(412)
+    ax2.set_title("VAD Output")
+    plt.plot(x_axis, vad_results)
+    ax3 = plt.subplot(413)
+    ax3.set_title("Gain Ch0")
+    # realgain = [x * round(vad_results[i]) for i, x in enumerate(gain)]
+    plt.plot(x_axis, gain0)
+    ax3 = plt.subplot(414)
+    ax3.set_title("Gain Ch1")
+    plt.plot(x_axis, gain1)
+    plt.show()
