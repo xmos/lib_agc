953e7518b1cdd1ba63a9e2250f7092a2c75b70a7
Brennan Magee <brennan@xmos.com>
2020-05-28 14:15:25 +0100

    Update setup.py deps

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,19 @@
-# Copyright (c) 2019, XMOS Ltd, All rights reserved
+# Copyright (c) 2019-2020, XMOS Ltd, All rights reserved
 import setuptools
 
 setuptools.setup(
     name='lib_agc',
     packages=setuptools.find_packages(),
-        install_requires=[
+    install_requires=[
         'audio_test_tools',
         'lib_vad',
         'lib_voice_toolbox',
         'numpy',
         'scipy',
         'matplotlib',
+        'pytest',
+        'pytest-xdist',
+        'webrtcvad',
     ],
     dependency_links=[
         './../../audio_test_tools/python#egg=audio_test_tools',
